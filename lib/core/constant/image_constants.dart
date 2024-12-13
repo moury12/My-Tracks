e8861e9527d44f0eef409c510043e5351e0b991b
moury12 <tanzibamouri00@gmail.com>
2024-12-13 11:13:31 +0600

    feat: static string in app

--- lib/core/constant/image_constants.dart
+++ lib/core/constant/image_constants.dart
@@ -2,3 +2,36 @@ String  splashImgUrl='assets/splash_img.png';
 String  loginImgUrl='assets/login_img.png';
 String  loginImg2Url='assets/login_img_2.png';
 String  dummyEventImgUrl='assets/dummy_event.png';
+
+
+///---------------------Icons------------------------------///
+///
+///
+String arrowTopImgUrl ='assets/icons/arrow_top.png';
+String addPhotoIconUrl  ='assets/icons/add_photo_icon.png';
+String addIconUrl  ='assets/icons/add_icon.png';
+String editIconUrl  ='assets/icons/edit_icon.png';
+String deleteIconUrl  ='assets/icons/delete_icon.png';
+String commentIconUrl  ='assets/icons/comment.png';
+String calenderIconUrl  ='assets/icons/calender_icon.png';
+String bookingIconUrl  ='assets/icons/booking_icon.png';
+String feedBackIconUrl  ='assets/icons/feedback.png';
+String drawerIconUrl  ='assets/icons/drawer_icon.png';
+String mapIconUrl  ='assets/icons/map.png';
+String manageIconUrl  ='assets/icons/manage_icon.png';
+String logoutIconUrl  ='assets/icons/logout.png';
+String locationIconUrl  ='assets/icons/location.png';
+String homeIconUrl  ='assets/icons/home_icon.png';
+String historyIconUrl  ='assets/icons/history.png';
+String promoteIconUrl  ='assets/icons/remote_icon.png';
+String reactIconUrl  ='assets/icons/react_icon.png';
+String profileIconUrl  ='assets/icons/profile_icon.png';
+String privacyPolicyIconUrl  ='assets/icons/privacy_policy.png';
+String photoIconUrl  ='assets/icons/photo_icon.png';
+String notificationIconUrl  ='assets/icons/notification_icon.png';
+String userLocationIconUrl  ='assets/icons/user_location.png';
+String termsConditionIconUrl  ='assets/icons/terms_condition.png';
+String shareIconUrl  ='assets/icons/share.png';
+String settingsIconUrl  ='assets/icons/settings.png';
+String searchIconUrl  ='assets/icons/search_icon.png';
+String IconUrl  ='';
