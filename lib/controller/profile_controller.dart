3903e691ff5043ce14ac0b5cc4ce6bc26fc89f0d
moury12 <tanzibamouri00@gmail.com>
2024-12-28 18:09:10 +0600

    feat:user info controller

--- lib/controller/profile_controller.dart
+++ lib/controller/profile_controller.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:track_trek/core/model/user_model.dart';
+import 'package:track_trek/core/service/user_service.dart';
 
 class ProfileController extends GetxController {
   static ProfileController get to => Get.find();
-
+  RxBool loadingUserData =false.obs;
+Rx<UserModel> userModel =UserModel().obs;
   ///=====================add dynmic name ====================///
   Rx<TextEditingController> nameController =
       TextEditingController(text: 'Khushi akter').obs;
@@ -20,6 +23,11 @@ class ProfileController extends GetxController {
   Rx<TextEditingController> locationController =
       TextEditingController(text: '4140 Parker Rd. Allentown, New Mexico 31134')
           .obs;
+  getUserProfileData()async{
+    loadingUserData.value = true;
+    userModel.value = await UserService.getUserData();
+    loadingUserData.value=false;
+  }
   @override
   void onClose() {
     nameController.value.dispose();
