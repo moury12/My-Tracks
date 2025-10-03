50e3f9826c88b4a2a12b0ed45225248a10a26928
moury12 <tanzibamouri00@gmail.com>
2025-10-03 18:16:41 +0600

    feat: apple rejection fixed

--- lib/core/service/track-event/track_event_service.dart
+++ lib/core/service/track-event/track_event_service.dart
@@ -270,7 +270,7 @@ class TrackEventService {
       final headers = {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ${Boxes.getUserData().get(tokenKey)}',
+        if( Boxes.getUserData().get(tokenKey)!=null) 'Authorization': 'Bearer ${Boxes.getUserData().get(tokenKey)}',
       };
 
       final response = await http.get(
